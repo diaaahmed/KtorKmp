7906b7eb5e862c374a00a8d4f0cbbe43fad98a3a
diaa <diaaahmedh200@gmail.com>
2025-01-06 20:26:15 +0200

    Add BottomNavigation

--- composeApp/src/commonMain/kotlin/org/ktor/ktorkmp/app/tabs/BankTab.kt
+++ composeApp/src/commonMain/kotlin/org/ktor/ktorkmp/app/tabs/BankTab.kt
@@ -6,6 +6,7 @@ import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import ktorkmp.composeapp.generated.resources.Res
+import ktorkmp.composeapp.generated.resources.ic_bank_new
 import ktorkmp.composeapp.generated.resources.ic_home
 import org.jetbrains.compose.resources.painterResource
 import org.ktor.ktorkmp.app.screens.bankPrice.BankScreen
@@ -21,7 +22,7 @@ object BankTab: Tab {
     override val options: TabOptions
         @Composable
         get() {
-            val icon = painterResource(Res.drawable.ic_home)
+            val icon = painterResource(Res.drawable.ic_bank_new)
 
             return remember {
                 TabOptions(
--- composeApp/src/commonMain/kotlin/org/ktor/ktorkmp/app/tabs/GoldTab.kt
+++ composeApp/src/commonMain/kotlin/org/ktor/ktorkmp/app/tabs/GoldTab.kt
@@ -7,6 +7,7 @@ import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import ktorkmp.composeapp.generated.resources.Res
 import ktorkmp.composeapp.generated.resources.ic_explore
+import ktorkmp.composeapp.generated.resources.ic_gold_new_item
 import org.jetbrains.compose.resources.painterResource
 import org.ktor.ktorkmp.app.screens.goldprice.GoldScreen
 
@@ -21,7 +22,7 @@ object GoldTab: Tab {
     override val options: TabOptions
         @Composable
         get() {
-            val icon = painterResource(Res.drawable.ic_explore)
+            val icon = painterResource(Res.drawable.ic_gold_new_item)
 
             return remember {
                 TabOptions(
